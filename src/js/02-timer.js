45eb847b5149c46dea295571af6a51269e59e546
Denis Moskalenko <87602096+DeniOS777@users.noreply.github.com>
2021-12-26 19:05:27 +0200

    Update 02-timer.js

---
+++ src/js/02-timer.js
@@ -0,0 +1,59 @@
+import flatpickr from 'flatpickr';
+
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    if (selectedDates[0].getTime() < Date.now()) {
+      refs.buttonStart.setAttribute('disabled', 'disabled');
+      return console.log('Дата меньше');
+    }
+    refs.buttonStart.removeAttribute('disabled');
+  },
+};
+
+const fp = flatpickr('#datetime-picker', options);
+console.log(fp.now);
+
+const refs = {
+  buttonStart: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+refs.buttonStart.addEventListener('click', onButtonStartTimerClick);
+
+refs.buttonStart.setAttribute('disabled', 'disabled');
+
+function onButtonStartTimerClick() {
+  console.log('Таймер до распродажи запущен');
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
