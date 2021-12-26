8e53692347a29d9734cd2489566250fb884791bd
Denis Moskalenko <87602096+DeniOS777@users.noreply.github.com>
2021-12-26 13:28:36 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  buttonStart: document.querySelector('[data-start]'),
+  buttonStop: document.querySelector('[data-stop]'),
+};
+
+const INTERVAL_DELAY = 1000;
+let intervalID = null;
+let hasIntervalRun = false;
+
+function onButtonStartChangeColorBodyClick() {
+  if (hasIntervalRun) {
+    return;
+  }
+  intervalID = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    console.log('Start');
+  }, INTERVAL_DELAY);
+  hasIntervalRun = true;
+}
+
+function onButtonStopChangeColorBodyClick() {
+  clearInterval(intervalID);
+  hasIntervalRun = false;
+  console.log('Stop');
+}
+
+refs.buttonStart.addEventListener('click', onButtonStartChangeColorBodyClick);
+refs.buttonStop.addEventListener('click', onButtonStopChangeColorBodyClick);
