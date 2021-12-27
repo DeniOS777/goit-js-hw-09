1f38bdc26acbf4beafcdfc834d980daad0195a28
Denis Moskalenko <87602096+DeniOS777@users.noreply.github.com>
2021-12-27 03:46:42 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -49,12 +49,31 @@ class Timer {
         return Report.success('SALE', 'Sale started!!!', 'Okay');
       }
       const deltaTime = selectedDate - Date.now();
-      const formatComponents = convertMs(deltaTime);
-      this.onShow(formatComponents);
+      const formatedDate = this.convertMs(deltaTime);
+      console.log(formatedDate);
+      this.onShow(formatedDate);
     }, this.intervalId);
 
     refs.buttonStart.setAttribute('disabled', 'disabled');
     refs.input.setAttribute('disabled', 'disabled');
+  }
+
+  convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = this.addLeadingZero(Math.floor(ms / day));
+    const hours = this.addLeadingZero(Math.floor((ms % day) / hour));
+    const minutes = this.addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    const seconds = this.addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+    return { days, hours, minutes, seconds };
+  }
+
+  addLeadingZero(value) {
+    return value.toString().padStart(2, '0');
   }
 }
 
@@ -69,22 +88,4 @@ function updateTimerInterface({ days, hours, minutes, seconds }) {
   refs.hours.textContent = `${hours}`;
   refs.minutes.textContent = `${minutes}`;
   refs.seconds.textContent = `${seconds}`;
-}
-
-function convertMs(ms) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  const days = addLeadingZero(Math.floor(ms / day));
-  const hours = addLeadingZero(Math.floor((ms % day) / hour));
-  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
-
-  return { days, hours, minutes, seconds };
-}
-
-function addLeadingZero(value) {
-  return value.toString().padStart(2, '0');
 }
